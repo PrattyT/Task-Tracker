523c0e633a15cf12791ef8d85a584b10bab7bb90
Pratyush Tulsian <pratyushtulsian@gmail.com>
2021-05-19 21:51:08 -0400

    added styling

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,29 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+
+const Header = ({title}) => {
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className = 'header'>
+            <h1 >{title}</h1>
+            <button className = 'btn'>
+                Add
+            </button>
         </header>
     )
 }
+
+Header.defaultProps =  {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+
+// css in js
+// const headingStyle = {
+//     color:'red', 
+//     backgroundColor: 'black'
+// }
 
 export default Header
