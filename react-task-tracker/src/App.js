edc08dc014ebc2af11fba70040abdcf924861da8
Pratyush Tulsian <pratyushtulsian@gmail.com>
2021-05-24 21:40:16 -0400

    created task comp

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,10 +1,30 @@
+
+import {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+
 import './App.css';
 
 function App() {
+   const [tasks, setTasks] = useState([
+{
+    id: 1,
+    text: 'Dcotors appointment',
+    day: 'feb 5',
+    reminder: true,
+},
+{
+    id: 2,
+    text: 'meeting at school',
+    day: 'feb 6',
+    reminder: false,
+},
+])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks= {tasks}/>
     </div>
   );
 }
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -6,11 +6,11 @@ const Header = ({title}) => {
     const onClick = () => {
         console.log('Click')
     }
-    
+
     return (
         <header className = 'header'>
             <h1 >{title}</h1>
-            <Button color = 'green' text = 'Add' onClick = {onClick}/>
+            <Button color = 'blue' text = 'Add' onClick = {onClick}/>
         </header>
     )
 }
